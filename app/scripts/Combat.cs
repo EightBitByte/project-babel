7e9603e85c5d04f5c0d1f4ca63d197d152732f47
EightBitByte <apixelatedpug@gmail.com>
2024-04-13 01:31:13 -0700

    :recycle: Refactor file paths to be constant strings

--- app/scripts/Combat.cs
+++ app/scripts/Combat.cs
@@ -1,27 +1,31 @@
-# define COMBAT_DEBUG
+// Define this flag for combat loading from JSON debug output.
+//# define COMBAT_LOAD_DEBUG
+# define COMBAT_LOG_DEBUG
 
 using System;
 using System.Collections.Generic;
 using Godot;
 
-
 public class Combat : Node
 {
+    const string PLAYER_ATTACK_FILE = "playerAttacks";
+    const string PLAYER_FILE = "player";
     const int DEFAULT_MAX_HP = 20;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         CombatPlayer cp = LoadCombatPlayer();
 
-        #if COMBAT_DEBUG
+        #if COMBAT_LOAD_DEBUG
         cp.DEBUG_PRINT();
         #endif
     }
 
     private CombatPlayer LoadCombatPlayer() {
         // Read the attack collection and player stats
-        Godot.Collections.Dictionary allAttacks = Json.ReadJSON("res://data/playerAttacks.json");
-        Godot.Collections.Dictionary playerDict = Json.ReadJSON("res://data/player.json");
+        Godot.Collections.Dictionary allAttacks = Json.ReadJSON("res://data/" + PLAYER_ATTACK_FILE + ".json");
+        Godot.Collections.Dictionary playerDict = Json.ReadJSON("res://data/" + PLAYER_FILE + ".json");
 
         Godot.Collections.Array knownAttacks = playerDict["knownAttacks"] as Godot.Collections.Array;
         int knownLength = knownAttacks.Count;
