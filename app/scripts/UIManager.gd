7b41b018a3af697207419e5e85188c9eb5c19e70
ailurada <132323791+ailurada@users.noreply.github.com>
2024-04-13 22:39:17 -0700

    re-added dialogue box

--- app/scripts/UIManager.gd
+++ app/scripts/UIManager.gd
@@ -1,12 +1,26 @@
 extends Node2D
 
+func toggle_inventory():
+	get_node("/root/Node2D/Camera2D/BigMenuBorder").toggle()
+	get_node("/root/Node2D/Camera2D/InventoryBackground").toggle()
+
+func show_inventory():
+	get_node("/root/Node2D/Camera2D/BigMenuBorder").show()
+	get_node("/root/Node2D/Camera2D/InventoryBackground").show()
+
+func hide_inventory():
+	get_node("/root/Node2D/Camera2D/BigMenuBorder").hide()
+	get_node("/root/Node2D/Camera2D/InventoryBackground").hide()
+
+
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.scancode == KEY_E:
-			get_node("/root/Node2D/Camera2D/BigMenuBorder").toggle()
-
+			toggle_inventory()
+			
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	hide_inventory()
 	pass # Replace with function body.
 
 
