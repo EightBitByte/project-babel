c8a3ce140900617ed08ad1f01f581f6f47b121de
Bryan Silva <bryanjsilva04@gmail.com>
2024-04-14 16:36:47 -0700

    Added basic enemy AI

---
+++ app/scripts/enemiesOverworld/EnemyOverworld.cs
@@ -0,0 +1,80 @@
+using Godot;
+using System;
+
+public class EnemyOverworld : KinematicBody2D
+{
+	private float m_speed = 300;
+	
+	const randomness = 0.1;
+	
+	private int tileWidth = 50;
+	private string currentMotion;
+	
+	private Godot.Collections.Dictionary<string, Vector2> directionToVector;
+	private Godot.Collections.Dictionary<string, bool> isDirOpen;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		m_speed *= Scale.x;
+		tileWidth = (int) (tileWidth * Scale.x);
+		
+		directionToVector = new Godot.Collections.Dictionary<string, Vector2> {
+			{"left", new Vector2(-1, 0)},
+			{"up", new Vector2(0, -1)},
+			{"right", new Vector2(1, 0)},
+			{"down", new Vector2(0, 1)}
+		};
+		
+		isDirOpen = new Godot.Collections.Dictionary<string, bool> {
+			{"left", false},
+			{"up", false},
+			{"right", false},
+			{"down", false}
+		};
+		
+		currentMotion = "right";
+	}
+	
+	private void _CheckCollisions() {
+		var spaceState = GetWorld2d().DirectSpaceState;
+		var rightColision = spaceState.IntersectRay(GlobalPosition, GlobalPosition + new Vector2(tileWidth, 0), new Godot.Collections.Array { this });
+		var downColision = spaceState.IntersectRay(GlobalPosition, GlobalPosition + new Vector2(0, tileWidth), new Godot.Collections.Array { this });
+		var leftColision = spaceState.IntersectRay(GlobalPosition, GlobalPosition + new Vector2(-tileWidth, 0), new Godot.Collections.Array { this });
+		var upColision = spaceState.IntersectRay(GlobalPosition, GlobalPosition + new Vector2(0, -tileWidth), new Godot.Collections.Array { this });
+		
+		isDirOpen["right"] = rightColision.Count == 0;
+		isDirOpen["down"] = downColision.Count == 0;
+		isDirOpen["left"] = leftColision.Count == 0;
+		isDirOpen["up"] = upColision.Count == 0;
+		
+		GD.Print(isDirOpen);
+	}
+	
+	// Chooses a random direction to move in from available directions
+	private string _ChooseAvailableDirection() {
+		Godot.Collections.Array<string> availableDirs = new Godot.Collections.Array<string>();
+		foreach (string dir in isDirOpen.Keys) {
+			if (isDirOpen[dir]) {
+				availableDirs.Add(dir);
+			}
+		}
+		
+		RandomNumberGenerator random = new RandomNumberGenerator();
+		random.Randomize();
+		float randomNum = random.Randf() * (availableDirs.Count);
+		return availableDirs[(int) randomNum];
+	}
+	
+	public override void _PhysicsProcess(float delta)
+	{
+		_CheckCollisions();
+		
+		if (!isDirOpen[currentMotion]) {
+			currentMotion = _ChooseAvailableDirection();
+		}
+		
+		MoveAndSlide(directionToVector[currentMotion] * m_speed);
+		_Draw();
+	}
+}
