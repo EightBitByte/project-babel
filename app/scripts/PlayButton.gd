7bf7e1dc8a783a238e25fd9dd540ca11a7c86ae6
ailurada <132323791+ailurada@users.noreply.github.com>
2024-04-13 23:53:35 -0700

    added title screen

--- app/scripts/PlayButton.gd
+++ app/scripts/PlayButton.gd
@@ -1,10 +1,16 @@
 extends "res://scripts/MenuButton.gd"
 
 
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.text = "save"
+	self.text = "play"
 	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
