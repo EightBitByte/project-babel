9915d73ce3bb54dca2cd4c464df432b18c936018
Bryan Silva <bryanjsilva04@gmail.com>
2024-04-14 02:58:49 -0700

    Added Interactable class and test collisions

---
+++ app/scripts/interactable/Interactable.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public class Interactable : Node
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+	
+	Label interactText;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		interactText = GetChild(2) as Label;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		if (Input.IsActionPressed("move_right") && interactText.Visible) {
+			GD.Print("yippee");
+		}
+	}
+
+	private void _on_Interactable_body_entered(object body)
+	{
+		interactText.Show();
+	}
+	
+	private void _on_Interactable_body_exited(object body)
+	{
+		interactText.Hide();
+	}
+}
