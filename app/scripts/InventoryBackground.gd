cce5ac38c7e712ac7ed93c42d908ba00de1dd02d
ailurada <132323791+ailurada@users.noreply.github.com>
2024-04-13 21:16:42 -0700

    added background fade

---
+++ app/scripts/InventoryBackground.gd
@@ -0,0 +1,23 @@
+extends ColorRect
+
+func update():
+	self.set_size(get_viewport_rect().size)
+
+	self.rect_position.x = -self.rect_size.x/2
+	self.rect_position.y = -self.rect_size.y/2
+
+func toggle():
+	if is_visible():
+		hide()
+	else:
+		show()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	update()
--- app/scripts/UIManager.gd
+++ app/scripts/UIManager.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
+func toggle_inventory_menu():
+	get_node("/root/Node2D/Camera2D/BigMenuBorder").toggle()
+	get_node("/root/Node2D/Camera2D/InventoryBackground").toggle()
+
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.scancode == KEY_E:
-			get_node("/root/Node2D/Camera2D/BigMenuBorder").toggle()
-
+			toggle_inventory_menu()
+			
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
