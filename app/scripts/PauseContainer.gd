6d0bd290e9cdc56bf4420438bbfa3d26a2d70742
ailurada <132323791+ailurada@users.noreply.github.com>
2024-04-14 18:16:54 -0700

    fixed pause

--- app/scripts/PauseContainer.gd
+++ app/scripts/PauseContainer.gd
@@ -1,20 +1,13 @@
 extends VBoxContainer
 
 func center_self():
-	self.rect_position.x = ( - self.rect_size.x)/2
-	self.rect_position.y = ( - self.rect_size.y)/2
-
-
-func toggle():
-	if is_visible():
-		hide()
-	else:
-		show()
-
+	self.rect_position.x = (get_viewport().size.x - self.rect_size.x)/2
+	self.rect_position.y = (get_viewport().size.y - self.rect_size.y)/2
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.alignment = ALIGN_CENTER
+	
 	pass # Replace with function body.
 
 
